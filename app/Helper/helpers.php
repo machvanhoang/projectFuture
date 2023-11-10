732700771cc9ebc6a9eb2db0982434800ccbc4f3
AGL_Mạch Văn Hoàng (ホアン) <81142674+machvanhoang@users.noreply.github.com>
2023-11-10 16:16:47 +0700

    Quantity

--- app/Helper/helpers.php
+++ app/Helper/helpers.php
@@ -52,3 +52,57 @@ if (!function_exists("response_success")) {
         return response()->json($response, $code);
     }
 }
+
+if (!function_exists('content_quality')) {
+    function content_quality(string $content, array $keywords)
+    {
+        $content = strtolower($content);
+        foreach ($keywords as $keyword) {
+            if (strpos($content, strtolower($keyword)) === false) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+if (!function_exists('calculate_readability')) {
+    function calculate_readability($content)
+    {
+        $wordCount = str_word_count($content);
+        $sentenceCount = preg_match_all('/[.!?]/u', $content);
+        $syllableCount = count_syllables($content);
+
+        $readability = 206.835 - 1.015 * ($wordCount / $sentenceCount) - 84.6 * ($syllableCount / $wordCount);
+
+        return $readability;
+    }
+}
+if (!function_exists('count_syllables')) {
+    function count_syllables($word)
+    {
+        $word = preg_replace('/[^aeiouy]+/i', '', $word);
+        return max(1, count(preg_split('/[aeiouy]+/i', $word)));
+    }
+
+}function calculateGunningFogIndex($content) {
+    $wordCount = str_word_count($content);
+    $sentenceCount = preg_match_all('/[.!?]/u', $content);
+    $complexWordCount = countComplexWords($content);
+
+    $gunningFogIndex = 0.4 * (($wordCount / $sentenceCount) + 100 * ($complexWordCount / $wordCount));
+
+    return $gunningFogIndex;
+}
+
+function countComplexWords($content) {
+    preg_match_all('/\b\w{3,}\b/', $content, $matches);
+    return count($matches[0]);
+}
+
+// Sử dụng hàm tính toán Gunning Fog Index cho một đoạn văn bản
+$paragraph = "Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn. Lorem Ipsum đã được sử dụng như một văn bản chuẩn cho ngành công nghiệp in ấn từ những năm 1500, khi một họa sĩ vô danh ghép nhiều đoạn văn bản với nhau để tạo thành một bản mẫu văn bản. Đoạn văn bản này không những đã tồn tại năm thế kỉ, mà khi được áp dụng vào tin học văn phòng, nội dung của nó vẫn không hề bị thay đổi. Nó đã được phổ biến trong những năm 1960 nhờ việc bán những bản giấy Letraset in những đoạn Lorem Ipsum, và gần đây hơn, được sử dụng trong các ứng dụng dàn trang, như Aldus PageMaker.";
+$gunningFogIndex = calculateGunningFogIndex($paragraph);
+
+echo "Chỉ số Gunning Fog: " . round($gunningFogIndex, 2);
+die;
--- app/Http/Controllers/Api/SuggestionController.php
+++ app/Http/Controllers/Api/SuggestionController.php
@@ -4,45 +4,40 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class SuggestionController extends Controller
 {
+    /**
+     * @var string
+     */
     const SUGGESTION_URL = "https://www.google.com/complete/search?output=toolbar&json=true&q=";
     public function index(Request $request)
     {
         $originalString = "google ads";
-        $encodedString = urlencode($originalString);
+        $encodedString = str_replace(" ", "%20", $originalString);
         $url = self::SUGGESTION_URL . $encodedString;
-        $response = Http::get($url);
-        $xmlData = $response->body();
-
-        // Clean the XML content and ensure proper encoding
-        $cleanedXmlData = $this->cleanXmlString($xmlData);
-
-        // Parse XML using SimpleXML
-        $xml = simplexml_load_string($cleanedXmlData);
-
-        // Convert SimpleXML object to an array
-        $suggestions = json_decode(json_encode($xml), true);
-
+        $suggestions = $this->getSuggestions($url);
         return response_success([
             "suggestions" => $suggestions,
         ], __('suggestion.success'));
     }
+
     /**
-     * Summary of cleanXmlString
-     * @param mixed $xmlString
-     * @return array|string|null
+     * Summary of getSuggestions
+     * @param string $url
+     * @return array
      */
-    private function cleanXmlString($xmlString)
+    private function getSuggestions(string $url)
     {
-        // Remove invalid UTF-8 characters
-        $cleanedXmlString = iconv('UTF-8', 'UTF-8//IGNORE', $xmlString);
+        $html = file_get_contents($url);
+        $html = mb_convert_encoding($html, 'UTF-8', mb_detect_encoding($html, 'UTF-8, ISO-8859-1', true));
+        $xml = new \SimpleXMLElement($html);
+        $suggestions = [];
 
-        // Replace invalid characters with the Unicode replacement character
-        $cleanedXmlString = preg_replace('/[^\x{0009}\x{000a}\x{000d}\x{0020}-\x{D7FF}\x{E000}-\x{FFFD}]/u', "\xEF\xBF\xBD", $cleanedXmlString);
-
-        return $cleanedXmlString;
+        foreach ($xml->CompleteSuggestion as $suggestion) {
+            $data = (string) $suggestion->suggestion['data'];
+            $suggestions[] = $data;
+        }
+        return $suggestions;
     }
 }
